92bc82227b2d05342e321d1f058758057060fe28
PrzemyslawPolrolniczak <przemekp96@gmail.com>
2018-06-27 00:09:03 +0200

    starting doing footer

--- footer.php
+++ footer.php
@@ -10,6 +10,22 @@
  */
 ?>
 
+<footer class="container-fluid">
+    <ul>
+        <li><span>&copy;2018</span> Przemysław Półrolniczak</li>
+        <li><a href="">[] Facebook</a></li>
+        <li><a href="">[] Twitter</a></li>
+        <li><a href="">[] Tel</a></li>
+        <li><a href="">[] Mail</a></li>
+
+    </ul>
+    <!-- <div class="row">
+        <div class="col-sm-4">ba</div>
+        <div class="col-sm-4">b</div>
+        <div class="col-sm-4">c</div>
+    </div> -->
+</footer>
+
 <?php wp_footer(); ?>
 </body>
 </html>
--- index.php
+++ index.php
@@ -8,6 +8,10 @@ get_header(); ?>
     <h1 class="text-center">Wynajem busów</h1>
 </div>
 <div class="container">
-  <div class="test"></div>
+    <div class="row">
+      <div class="col-sm-4">.col-sm-4</div>
+      <div class="col-sm-4">.col-sm-4</div>
+      <div class="col-sm-4">.col-sm-4</div>
+    </div>
 </div>
 <?php get_footer(); ?>
