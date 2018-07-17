0b45beeef63e60f785c00c3bdad4cc0bfadb5484
PrzemyslawPolrolniczak <przemekp96@gmail.com>
2018-07-17 20:55:46 +0200

    added busses section

--- index.php
+++ index.php
@@ -23,33 +23,35 @@ get_header(); ?>
             <!-- <img src="<?php echo get_template_directory_uri(); ?>/images/heart.png" alt=""> -->
         </div>
     </div>
-    <div class="container posts-section">
-        <?php $wpb_all_query = new WP_Query(array('post_type'=>'post', 'post_status'=>'publish', 'posts_per_page'=>3)); ?>
-        
-            <?php if ( $wpb_all_query->have_posts() ) : $postCount = 0; ?>
-            <div class="posts-title"><h1>Aktualności</h1> <a href="">Zobacz wszystkie</a></div>
-            <div class="posts row">
-                <!-- the loop -->
-                <?php while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); $postCount++; ?>
-                    
-                        <div class="post <?php if ($postCount == 1) : echo 'newest'; endif ?> col-sm-4">
-                            <span class="date"><?php echo get_the_date(); ?></span>
-                            <?php if ( has_post_thumbnail() ) { ?>
-                                <div class="text-center img-thumbnail">
-                                    <?php  the_post_thumbnail('thumbnail'); ?>
-                                </div>
-                            <?php } ?> 
-                            <h5><?php the_title(); ?></h5>
-                            <p><?php the_excerpt(); ?></p>
-                            <a href="<?php the_permalink(); ?>">Zobacz więcej <span class="triangle"></span></a>
-                        </div>
-                <?php endwhile; ?>
-                <!-- end of the loop -->
+    <div class="post-section-background">
+        <div class="container posts-section">
+            <?php $wpb_all_query = new WP_Query(array('post_type'=>'post', 'post_status'=>'publish', 'posts_per_page'=>3)); ?>
             
-            </div>
-        
-            <?php wp_reset_postdata(); ?>
-        <?php endif; ?>
+                <?php if ( $wpb_all_query->have_posts() ) : $postCount = 0; ?>
+                <div class="posts-title"><h1>Aktualności</h1> <a href="">Zobacz wszystkie</a></div>
+                <div class="posts row">
+                    <!-- the loop -->
+                    <?php while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); $postCount++; ?>
+                        
+                            <div class="post <?php if ($postCount == 1) : echo 'newest'; endif ?> col-sm-4">
+                                <span class="date"><?php echo get_the_date(); ?></span>
+                                <?php if ( has_post_thumbnail() ) { ?>
+                                    <div class="text-center img-thumbnail">
+                                        <?php  the_post_thumbnail('thumbnail'); ?>
+                                    </div>
+                                <?php } ?> 
+                                <h5><?php the_title(); ?></h5>
+                                <p><?php the_excerpt(); ?></p>
+                                <a href="<?php the_permalink(); ?>">Zobacz więcej <span class="triangle"></span></a>
+                            </div>
+                    <?php endwhile; ?>
+                    <!-- end of the loop -->
+                
+                </div>
+            
+                <?php wp_reset_postdata(); ?>
+            <?php endif; ?>
+        </div>
     </div>
     <div class="about-us-background">
         <div class="container about-us-section">
@@ -59,19 +61,46 @@ get_header(); ?>
                 </div>
         </div>
     </div>
-    <div class="container our-vehicles-section" style="height: 500px;">
-        <div class="dummy-img">
-
+    <div class="buses-section">
+        <div class="container">
+        <h1 class="text-center">Nasze busy</h1>
+            <div class="row">
+                <div class="dummy-img col-sm-6">
+                    <h1>Bus #1</h1>
+                </div>
+                <div class="dummy-text col-sm-6">
+                    <p>LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMET</p>
+                </div>
+            </div>
         </div>
-        <div class="dummy-text">
-            <p>LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMET</p>
+        <div class="buses-background">
+            <div class="container">
+                <div class="row">
+                    <div class="dummy-text col-sm-6">
+                        <p>LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMET</p>
+                    </div>
+                    <div class="dummy-img col-sm-6">
+                        <h1>Bus #2</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="dummy-img col-sm-6">
+                    <h1>Bus #3</h1>
+                </div>
+                <div class="dummy-text col-sm-6">
+                    <p>LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMET</p>
+                </div>
+            </div>
         </div>
     </div>
     <div class="container pricing-section" style="height: 500px;">
-        PRICES
+        <h1 class="text-center">Ceny</h1>
     </div>
     <div class="google-map" style="height: 500px;">
-        GOOGLE MAP HERE
+        <iframe id="mapsource" width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=17.292300760746006%2C50.94065843612187%2C17.295841276645664%2C50.94227078914687&amp;layer=mapnik&amp;marker=50.94146461962363%2C17.29407101869583" style="border: 1px solid black"></iframe>
     </div>
 </div>
 <?php get_footer(); ?>
